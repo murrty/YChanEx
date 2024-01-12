a5fa6abd2a93fa1733a3ab4bba5171b52c68382e
matty <10895809+murrty@users.noreply.github.com>
2024-01-11 18:54:45 -0600

    [4chan] get ids that are replied to by this post

--- src/YChanEx/Classes/Post Objects/FourChanPost.cs
+++ src/YChanEx/Classes/Post Objects/FourChanPost.cs
@@ -1,13 +1,19 @@
 ﻿#nullable enable
 namespace YChanEx.Posts;
+using System.Diagnostics;
 using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
 [DataContract]
+[DebuggerDisplay("{no} | HasFile = {HasFile}")]
 internal sealed class FourChanPost {
+    // Regex
+    private static readonly Regex RepliesRegex = new("href=\"#p\\d+\"", RegexOptions.IgnoreCase);
+
     [DataMember(Name = "no")]
     public ulong no { get; set; }
 
-    //[DataMember(Name = "now")]
-    //public string? now { get; set; }
+    [DataMember(Name = "now")]
+    public string? now { get; set; }
 
     [DataMember(Name = "name")]
     public string? name { get; set; }
@@ -51,8 +57,8 @@ internal sealed class FourChanPost {
     [DataMember(Name = "spoiler")]
     public int spoiler { get; set; }
 
-    //[DataMember(Name = "resto")]
-    //public int? resto { get; set; }
+    [DataMember(Name = "resto")]
+    public int? resto { get; set; }
 
     [DataMember(Name = "id")]
     public string? id { get; set; }
@@ -63,32 +69,32 @@ internal sealed class FourChanPost {
     [DataMember(Name = "trip")]
     public string? trip { get; set; }
 
-    //[DataMember(Name = "bumplimit")]
-    //public int bumplimit { get; set; }
+    [DataMember(Name = "bumplimit")]
+    public int bumplimit { get; set; }
 
-    //[DataMember(Name = "imagelimit")]
-    //public int imagelimit { get; set; }
+    [DataMember(Name = "imagelimit")]
+    public int imagelimit { get; set; }
 
-    //[DataMember(Name = "semantic_url")]
-    //public string? semantic_url { get; set; }
+    [DataMember(Name = "semantic_url")]
+    public string? semantic_url { get; set; }
 
-    //[DataMember(Name = "custom_spoiler")]
-    //public int custom_spoiler { get; set; }
+    [DataMember(Name = "custom_spoiler")]
+    public int custom_spoiler { get; set; }
 
-    //[DataMember(Name = "replies")]
-    //public int replies { get; set; }
+    [DataMember(Name = "replies")]
+    public int replies { get; set; }
 
-    //[DataMember(Name = "images")]
-    //public int images { get; set; }
+    [DataMember(Name = "images")]
+    public int images { get; set; }
 
-    //[DataMember(Name = "unique_ips")]
-    //public int unique_ips { get; set; }
+    [DataMember(Name = "unique_ips")]
+    public int unique_ips { get; set; }
 
-    //[DataMember(Name = "sticky")]
-    //public int sticky { get; set; }
+    [DataMember(Name = "sticky")]
+    public int sticky { get; set; }
 
-    //[DataMember(Name = "closed")]
-    //public int closed { get; set; }
+    [DataMember(Name = "closed")]
+    public int closed { get; set; }
 
     [DataMember(Name = "archived")]
     public bool archived { get; set; }
@@ -101,6 +107,26 @@ internal sealed class FourChanPost {
     public bool HasFile {
         get {
             return !string.IsNullOrWhiteSpace(filename) && !string.IsNullOrWhiteSpace(ext) && !string.IsNullOrWhiteSpace(md5);
+        }
+    }
+
+    [IgnoreDataMember]
+    public ulong[]? RespondsTo {
+        get {
+            if (com.IsNullEmptyWhitespace()) {
+                return null;
+            }
+
+            var Matches = RepliesRegex.Matches(com);
+            if (Matches.Count < 1) {
+                return null;
+            }
+
+            return Matches
+                .Cast<Match>()
+                .Select(x => x.Value[8..^1])
+                .Select(ulong.Parse)
+                .ToArray();
         }
     }
 }
